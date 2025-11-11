de31eabf72ed7d83b358a7b66520fc8940f5da3c
AdrianGlanowski <adrianglanowski04@gmail.com>
2025-11-11 17:03:34 +0100

    added tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,23 @@
 from solution import Solution
+
+def test_case1():
+    solution = Solution("case1", True)
+    solution.solve()
+    with open("output/case1/case1.txt", "r") as f:
+        f.readline()
+        f.readline()
+        x = f.readline().strip()
+    assert x == "FNF(acdcfbbe) = (adb)(cb)(c)(fe)"
+
+def test_case2():
+    solution = Solution("case2", True)
+    solution.solve()
+    with open("output/case2/case2.txt", "r") as f:
+        f.readline()
+        f.readline()
+        x = f.readline().strip()
+    assert x == "FNF(afaeffbcd) = (af)(af)(ef)(b)(c)(d)"
+
 def test_case3():
     solution = Solution("case3", True)
     solution.solve()
@@ -7,3 +26,12 @@ def test_case3():
         f.readline()
         x = f.readline().strip()
     assert x == "FNF(baadcb) = (b)(ad)(a)(cb)"
+
+def test_case4():
+    solution = Solution("case4", True)
+    solution.solve()
+    with open("output/case4/case4.txt", "r") as f:
+        f.readline()
+        f.readline()
+        x = f.readline().strip()
+    assert x == "FNF(acdcfbbe) = (ad)(cf)(c)(be)(b)"
